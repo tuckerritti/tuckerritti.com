7f4b29f4c3f8627c106504a62fbe8191855f9c71
Tucker Ritti <45155694+tuckerritti@users.noreply.github.com>
2025-05-03 09:54:22 -0700

    github actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/tuckerritti.com/", // Set the base path for GitHub Pages deployment
 });
